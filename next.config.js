ade5488f9ec204216af8246f32d3a637ef4961b6
deepesh8216 <kumardeepesh542@gmail.com>
2025-02-21 21:01:36 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
+
+
+/*
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  
+ 
+//};
+
+//export default nextConfig;
+
